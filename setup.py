b69eb8511f4745fd35b906c860974d3d1a2a1202
Stephan Schrijver <stephanschrijver@gmail.com>
2020-05-21 13:28:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import cryptory
 
 
 setup(name='cryptory',
-      version=0.1.1,
+      version='0.1.1',
       url='https://github.com/dashee87/cryptory',
       author='David Sheehan',
       author_email='davidfsheehan87@gmail.com',
